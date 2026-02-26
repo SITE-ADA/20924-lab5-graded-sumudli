3842a37a69ed896cc6c45627a07249bb48dd5f0c
Student B012 <studentb012@ada.local>
2026-02-26 15:44:37 +0400

    Add EventController:GET/upcoming implementation

--- src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
+++ src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
@@ -127,4 +127,13 @@ public class EventController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    @GetMapping("/upcoming")
+    public ResponseEntity<List<Event>> getUpcomingEvents() {
+        try {
+            List<Event> events = eventService.getUpcomingEvents();
+            return new ResponseEntity<>(events, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
